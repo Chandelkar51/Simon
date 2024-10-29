2ef1845b5ae53244cc63b849241ad17948e2aed3
Chandelkar51 <nikhil05052001@gmail.com>
2024-10-29 20:48:24 +0530

    seq updation

--- game.js
+++ game.js
@@ -3,13 +3,12 @@ let scr=document.querySelector(".scr"); //score element
 let hscr=document.querySelector(".hscr");//highest score element
 let score=0;
 let Hscore=0;
-// let cur_seq;
 let seq=[]; // Game sequnce
 let btn=["d1", "d2", "d3", "d4"];
 let start=false;
 let sbtn=document.querySelector(".Sbtn");
-// starting or ending the game
 
+// starting or ending the game
 sbtn.addEventListener("click", function(){
     if(start==false)
         {
@@ -17,8 +16,6 @@ sbtn.addEventListener("click", function(){
             sbtn.innerText="Leave";
             scr.innerText="Score : 0";
             start=true;
-            // console.log("level up");
-            // flash(sbtn);
         levelup();
     }
     else
@@ -34,21 +31,32 @@ sbtn.addEventListener("click", function(){
     }
 });
 
-let index=0
+let index=0;
 function match(clickedbtn){
-if(!(clickedbtn==seq[index]))
-{
-    console.log("game over")
-}
-else{
-index++;
-if(index==seq.length)
-    {
-    console.log("good");
-    levelup();
-    index=0;
-}
-}
+    if(!(clickedbtn==seq[index]))
+        {
+            // console.log("game over")
+            seq=[];
+            index=0;
+            //Details after ending game......
+            sbtn.classList.remove("leave");
+            sbtn.innerText="Start";
+            if(Hscore<score){
+                Hscore=score-1;
+                hscr.innerText="Highiest Score: "+Hscore;
+            }
+            score=0;
+            start=false;
+        }
+    else{
+        index++;
+        if(index==seq.length)
+            {
+            // console.log("good");
+            levelup();
+            index=0;
+        }
+    }
 }
 
 
@@ -67,6 +75,6 @@ function levelup(){
     let rndmclr=btn[random];
     seq.push(rndmclr);
     let  fbtn=document.querySelector(`.${rndmclr}`);
-    // console.log(fbtn, rndmclr);
+    // console.log(fbtn);
     flash(fbtn);
 }
